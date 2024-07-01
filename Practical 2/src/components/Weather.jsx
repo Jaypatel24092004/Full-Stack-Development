f05139e5ad8578a3fade5db5f16f3f21f1168fd2
PATEL JAY JIGNESHKUMAR <jjpatel24092004@gmail.com>
2024-07-01 22:02:30 +0530

    prac2 started

--- Practical 2/src/components/Weather.jsx
+++ Practical 2/src/components/Weather.jsx
@@ -4,15 +4,12 @@ const Weather = () => {
   return (
     <div className='flex justify-center items-center w-full min-h-screen bg-center bg-cover' style={{backgroundImage:`url('https://res.cloudinary.com/dgwtgr6do/image/upload/v1719762617/folbwzv1jfp8vxs33ewf.jpg')`}}>
         <div className='rounded-xl bg-white shadow-lg w-4/5 h-4/5 opacity-70'>
-            <div className='text-3xl font-bold text-center flex items-center justify-center '>
+            <div className='text-6xl font-bold text-center flex items-center justify-center '>
                 <p> WEATHER APP </p>
             </div>
             <form>
-                <input
-                type='text'
-                placeholder='Enter the city Name'
-                required>
-                </input>
+                <label htmlFor="">Choose your city
+                </label>
             </form>
         </div>
 
